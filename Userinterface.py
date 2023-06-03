bb518aa1380512d445a0d2dbe55701a489509a9c
IrisOsp <irisjohannesdottir@gmail.com>
2023-06-03 16:52:46 +0200

    meget rå GUI med superklasser og knapper osv.

--- Userinterface.py
+++ Userinterface.py
@@ -44,13 +44,13 @@ class Menu(tk.Frame):
 
     def update_puls(self):
         value=round(puls.getVitals())
-        self.label2=tk.Label(self, text=value)
+        self.label2=tk.Label(self, text=value,width=20, height=5)
         self.label2.config(text=value)
         self.after(1000, self.update_puls)
         self.label2.grid(row=4, column=0, sticky='nswe',pady=2)
 
     def graf(self):
-        self.label_graf = tk.Label(self, text='Graf')
+        self.label_graf = tk.Label(self, text='Graf',width=20, height=5)
         self.label_graf.grid(row=3, column=2, sticky='nswe')
         self.entry = tk.Entry(self, width=5)
         self.entry.grid(row=4, column=2, sticky='nswe')
