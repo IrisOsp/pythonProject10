91e300d0b38850392fd3aa5d290f284e2e8221d9
roseliloelrich <roselil.oelrich@hotmail.com>
2023-06-08 13:45:53 +0200

    Hej

--- graf.py
+++ graf.py
@@ -1,34 +1,34 @@
-
-class sensor():
-    list = []
-    size = 0
-    max = 600
-    def load_data():
-        with open("H_data.txt", "r") as file:
-            linje=file.readline()
-
-
-
-
-
 class buffer():
     list = []
     size = 0
     max = 600
 
-
 class que():
     que = []
     size = 0  # lock
+def put(self.b):
     put()
     get()
-
     wait
     notify
 
+class sensor():
+    __init__(self,)
+    def load_data():
+        with open("H_data.txt", "r") as file:
+            linje = file.readline()
+
+
+b = buffer
+
+k=que()
+k1=que()
+s=sensor(k1)
+s.start()
+
+
 class db():
     tråd
-
 
 class graph():
     plot.....
