277341ae5354cba899efa0cde228fec2924107fe
NikolineSarahKurland <s184079@student.dtu.dk>
2023-06-09 11:05:01 +0200

    graf opdateret

--- graf.py
+++ graf.py
@@ -1,11 +1,20 @@
 import threading
 import time
+import tkinter as tk
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 class Buffer():
-    def __init__(self):
+    def __init__(self, que):
         self.buffer_list = []
         self.size = 0
         self.max_size = 600
+        self.que = que
+
+    def add_data(self, data):
+        self.buffer_list.append(data)
+        self.size += 1
+        self.que.put(data)
 
 
 class Que():
@@ -15,21 +24,22 @@ class Que():
         self.lock = threading.Lock()
 
     def put(self, obs):
-        self.lock.acquire()  # Henter lås før køen ændres
+        self.lock.acquire()
         self.que.append(obs)
         self.size += 1
-        self.lock.release()  # Slipper låsen efter ændring af køen
+        self.lock.release()
 
     def get(self):
-        self.lock.acquire()   # Henter lås før køen ændres
+        self.lock.acquire()
         if self.size == 0:
-            self.lock.release()  # Slipper låsen, hvis køen er tom
+            self.lock.release()
             return None
 
         obs = self.que.pop(0)
         self.size -= 1
         self.lock.release()
         return obs
+
 
 class Sensor(threading.Thread):
     def __init__(self, que):
@@ -39,12 +49,13 @@ class Sensor(threading.Thread):
     def load_data(self):
         with open("H_data.txt", "r") as file:
             line = file.readline()
-            self.que.put(line)  # Tilføj data til køen eller bufferen
+            self.que.put(float(line))  # Send data direkte til køen som float-værdi
 
     def run(self):
         while True:
             self.load_data()
-            time.sleep(0.5)  # Sov/vent i 0.5 sekund, før der indlæses de næste data
+            time.sleep(0.5)
+
 
 class DB(threading.Thread):
     def __init__(self, que):
@@ -57,27 +68,61 @@ class DB(threading.Thread):
             if obs is not None:
                 # Lagre data i databasen
                 pass
-            time.sleep(0.1)  # Sov/vent i 0.1 sekunder, før der behandles det næste i køen
+            time.sleep(0.1)
+
 
 class Graph():
-    def __init__(self, buffer):
-        self.buffer = buffer
+    def __init__(self, que):
+        self.que = que
+        self.data = []
 
     def plot_graph(self):
-        # Plotter grafen fra data fra Bufferen
-        pass
+        obs = self.que.get()
+        if obs is not None:
+            self.data.append(obs)  # Gem data til plotning
 
-buffer = Buffer()
-que = Que()
-sensor = Sensor(que)
-db = DB(que)
-graph = Graph(buffer)
+            # Plot grafen ved hjælp af Matplotlib
+            plt.clf()  # Clear the previous plot
+            plt.plot(self.data)
 
-sensor.start()
-db.start()
 
-# Fortsætter med at køre grafen i hovedtråden
-while True:
-    graph.plot_graph()
-    time.sleep(1)  # Sov/vent 1 sekund, før der plottes den næste måling/observation
+class MyGraph(tk.Frame):
+    def __init__(self, master):
+        tk.Frame.__init__(self, master)
+        self.master = master
+        self.que = Que()
+        self.graph = Graph(self.que)
 
+        self.figure = plt.figure(figsize=(8, 6))
+        self.ax = self.figure.add_subplot(111)
+        self.canvas = FigureCanvasTkAgg(self.figure, master=self)
+        self.canvas.get_tk_widget().pack(fill=tk.BOTH, expand=True)
+
+    def update_graph(self):
+        self.graph.plot_graph()
+        self.canvas.draw()
+        self.after(500, self.update_graph)
+
+
+def main():
+    root = tk.Tk()
+
+    canvas = MyGraph(root)
+    canvas.pack(fill="both", expand=True)
+
+    sensor = Sensor(canvas.que)
+    db = DB(canvas.que)
+
+    # Start de andre tråde
+    sensor.start()
+    db.start()
+
+    # Start opdatering af grafen
+    canvas.update_graph()
+
+    # Start hovedloop
+    root.mainloop()
+
+
+if __name__ == '__main__':
+    main()
