2c02aefb1fcd60f9958c3253101bfd5e81a36cbc
NikolineSarahKurland <s184079@student.dtu.dk>
2023-06-15 09:29:04 +0200

    læser data fra arduino

--- read_data.py
+++ read_data.py
@@ -6,7 +6,7 @@ baud_rate = 38400  # samme baudrate, som er angivet i Arduino-koden
 
 ser = serial.Serial(arduino_port, baud_rate, timeout=1)
 
-file_path = 'TEST_data.txt'  # Angiv stien til din tekstfil
+file_path = 'TEST_data.txt'  # Angiv stien
 
 file = open(file_path, 'w')
 
@@ -16,9 +16,9 @@ ser.setDTR(run)
 
 while run:
     try:
-        data = ser.readline().decode().strip()  # Læs en linje fra seriel forbindelse og dekod den til en streng
+        data = ser.readline().decode().strip()  # Læs en linje fra seriel forbindelse og omkod den til en streng
         if data:
-            file.write(data + '\n')  # Gem data i tekstfilen med en ny linje
+            file.write(data + '\n')  # Gemmer data i tekstfilen med en ny linje
             print(data)  # Udskriv dataene
         else:
             print(".", end="")
